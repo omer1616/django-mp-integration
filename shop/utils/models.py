29b80a7b5d0dd868e69b89a1700223b590bed79b
ömer inci <omerinci@omer-MacBook-Air.local>
2024-08-04 23:36:52 +0300

    delete attribute mixin

--- shop/utils/models.py
+++ shop/utils/models.py
@@ -6,16 +6,3 @@ class StartedModel(models.Model):
 
     class Meta(object):
         abstract = True
-
-
-class AttributeExtraMixin(models.Model):
-    is_required = models.BooleanField(default=False)
-    is_visible = models.BooleanField(default=True)
-    is_searchable = models.BooleanField(default=True)
-    is_filterable = models.BooleanField(default=False)
-    is_variant = models.BooleanField(default=False)
-    is_listable = models.BooleanField(default=False)
-    is_form_required = models.BooleanField(default=False)
-
-    class Meta:
-        abstract = True
